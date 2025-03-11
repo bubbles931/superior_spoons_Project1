53c46c68a6a611b4ca89e108a8734d18a4593659
firewalkwithme-netizen <bryon_villacorta@brown.edu>
2025-03-11 13:17:47 -0400

    loose_cards, pass or keep,
    made an array to create all the loose_cards worked on pass or keep and started to figure out how to use cell arrays to contain strings of diff lengths into columns so that we can call it for keep

--- spoons.m
+++ spoons.m
@@ -27,6 +27,8 @@ disp(player_array)
 
 suit = '';
 num = '';
+
+player_array_card_names = string(0);
 %for loop to determine what each player was dealt
 for each_player = 1:size(player_array, 1)
        disp(['Player ', num2str(each_player)]);
@@ -55,9 +57,14 @@ for each_player = 1:size(player_array, 1)
         end
         card_num = num2str(player_array_col);
         specific_cards = strcat(card_num, ". ", num, " of ",suit);
+        player_array_card_names(end+1) = specific_cards;
         disp(specific_cards)
+        
     end
 end
+%cellstr(player_array_card_names);
+%player_array_card_names = (reshape(player_array_card_names, size(player_array, 1), size(player_array, 2)));
+%disp(player_array_card_names)
 
 %FIND NUMBERS REMAINING IN 1:52 NOT DEALT 
 remaining_card = card_deck;
@@ -107,34 +114,48 @@ remaining_deck_array = reshape(remaining_card, 13, 4);
        end
        
     end
-disp(stored_names)
 
 
 %individualizing players using while loop
 current_player = 1;
 
+loose_cards = strings(0);
 while current_player ~= n_players
 %displaying a card to player 1
     index_stored_names = randi([1 length(stored_names)]);
     current_card = stored_names(index_stored_names);
     disp(current_card);
+    stored_names(index_stored_names) = [];
+        %remove current card from stored names
     %user input either pass or keep
     prompt = "pass or keep?";
     txt = input(prompt, 's');
    %conditional statement,if keep, then which card to remove
     if strcmp(txt, 'pass')
+        loose_cards(end+1) = current_card;
         while strcmp(txt, 'pass')
            index_stored_names = randi([1 length(stored_names)]);
            current_card = stored_names(index_stored_names);
            disp(current_card);
            prompt = "pass or keep?";
            txt = input(prompt, 's');
+           loose_cards(end+1) = current_card;
         end
     elseif strcmp(txt, 'keep')
-        prompt = 'Which card would you like to discard? (';
-        txt = input(prompt, 's');
-    else
+        prompt = 'Which card would you like to discard: 1, 2, 3, or 4?';
+        x = input(prompt);
+        discard_card = player_array(current_player, x);
 
+      %loose_cards(end+1) = 
+
+       
+
+
+            %swap current card to loose card, loose card goes into
+            %loose_card; update player_array
+    else
+       prompt = "Ensure that you type the command specificially: again, pass or keep?";
+       txt = input(prompt, 's');
     end
     current_player = current_player + 1;
 end
@@ -142,8 +163,16 @@ end
 
 end
 
+% make new array with cards in play
+
+
+% update player array and stored names if kept 
+% go through the indices of this new array for the rest of the players to use
+% once the player array and stored names is empty, make feeder use new array
+
 %next time
 %fix pass or keep so every time a card is drawn we update the stored_names
+%finish the feeder code, worry about trash and intermediate layer
 %list
 %finish keep and pass
 
